f2ca48b4271d7f9665f2c75ad65af8264c0c60b8
epistrephein <epistrephein@gmail.com>
2016-12-22 18:05:34 +0100

    added user and myitasa methods

--- lib/italiansubs.rb
+++ lib/italiansubs.rb
@@ -12,6 +12,14 @@ module ItalianSubs
   class APIError < StandardError; end
 
   class API
+    attr_accessor :authcode
+
+    def initialize(username = nil, password = nil)
+      return unless username.nil? || password.nil?
+      login!(username, password)
+      sleep 1
+    end
+
     # list of shows
     def shows
       res = call(
@@ -87,6 +95,85 @@ module ItalianSubs
       return [] if count.zero?
 
       res.body['Itasa_Rest2_Server_Subtitles']['search']['subtitles'].values
+    end
+
+    # user login
+    def login!(username, password)
+      res = call(
+        'users/login',
+        'username' => username,
+        'password' => password
+      )
+
+      @authcode =
+        res.body['Itasa_Rest2_Server_Users']['login']['user']['authcode']
+    end
+
+    # user profile
+    def user_profile
+      raise APIError, 'not logged in' if @authcode.nil?
+      res = call(
+        'users',
+        'authcode' => @authcode
+      )
+
+      res.body['Itasa_Rest2_Server_Users']['direct']['user']
+    end
+
+    # list myItasa shows
+    def myitasa_shows
+      res = call(
+        'myitasa/shows',
+        'authcode' => @authcode
+      )
+
+      res.body['Itasa_Rest2_Server_Myitasa']['shows']['shows'].values
+    end
+
+    # show last myItasa subtitles
+    def myitasa_last_subtitles(page = nil)
+      res = call(
+        'myitasa/lastsubtitles',
+        'authcode' => @authcode,
+        'page' => page
+      )
+
+      res.body['Itasa_Rest2_Server_Myitasa']['lastsubtitles']['subtitles'].values
+    end
+
+    # show myItasa next episodes
+    def myitasa_next_episodes(page = nil)
+      res = call(
+        'myitasa/nextepisodes',
+        'authcode' => @authcode,
+        'page' => page
+      )
+
+      res.body['Itasa_Rest2_Server_Myitasa']['nextepisodes']['episodes'].values
+    end
+
+    # add a show to myItasa
+    def myitasa_add_show(show_id, version)
+      res = call(
+        'myitasa/addShowToPref',
+        'authcode' => @authcode,
+        'show_id' => show_id,
+        'version' => version
+      )
+
+      res.body['Itasa_Rest2_Server_Myitasa']['addShowToPref']['shows'].values
+    end
+
+    # remove a show from myItasa
+    def myitasa_remove_show(show_id, version)
+      res = call(
+        'myitasa/removeShowFromPref',
+        'authcode' => @authcode,
+        'show_id' => show_id,
+        'version' => version
+      )
+
+      res.body['Itasa_Rest2_Server_Myitasa']['removeShowFromPref']['shows'].values
     end
 
     private
